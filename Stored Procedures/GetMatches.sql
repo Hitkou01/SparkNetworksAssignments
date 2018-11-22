95afdbe74a0d5b7662e41cf13b1586e30f3c60b0
Hitesh Koul <45242026+Hitkou01@users.noreply.github.com>
2018-11-22 17:18:48 +0000

    Add files via upload

---
+++ Stored Procedures/GetMatches.sql
@@ -0,0 +1,28 @@
+ALTER PROCEDURE GetMatches 
+	@UserId INT
+   ,@Action NVARCHAR(20) = N'GetMatchById'
+AS
+BEGIN
+	SET NOCOUNT ON;
+	IF @Action = N'GetMatchById'
+	BEGIN
+		SELECT 
+			U.[UserName]
+		   ,U.[Age]
+		   ,U.[Height_cms]
+		   ,U.[JobTitle]
+		   ,R.[ReligionName]
+		   ,U.[PokemonCatchRate]
+		   ,U.[IsCatLoving]
+		   ,U.[CatsOwnd]	
+		FROM dbo.[MatchedUsers] MU
+		INNER JOIN dbo.[Users] U
+			ON MU.[MatchedUserID] = U.UserId
+		INNER JOIN dbo.[Religion] R
+			ON U.[ReligionId] = R.[ReligionId]
+		WHERE MU.SourceUserID = @UserId
+		AND MU.[RejectedDate] IS NULL
+	END
+
+    
+END
